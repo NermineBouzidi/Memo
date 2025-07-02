5b01452b818cb6013a57772aefb3b6c82b3a7ff8
syrine zedini <syrinezeddini097@gmail.com>
2025-07-02 11:55:38 +0100

    reset-pass-flow review-task adding-adminRouter

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
 import userRouter from './routes/userRoutes.js';
 import authRouter from './routes/authRoutes.js';
+import statistiquesRouter from './routes/AdminRouter.js';
 
 import reviewRouter from './routes/reviewRoutes.js';
 import dotenv from 'dotenv';
@@ -29,6 +30,8 @@ app.use('/api/products', productRouter);
 
 //for review
 app.use('/api/review', reviewRouter);
+//for statistiques 
+app.use('/api/prod', statistiquesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
