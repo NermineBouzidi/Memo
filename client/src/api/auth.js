4c3a8069c61bd038afb50c42952bb812fb86476e
Nermine Bouzidi <101474434+NermineBouzidi@users.noreply.github.com>
2025-07-29 11:41:59 +0100

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a reusable Axios instance
 const api = axios.create({
-  baseURL: 'https://memo-1ppr.onrender.com/api/auth', // ✅ Change if your backend runs on a different port or subdomain
+  baseURL: 'https://memo-back.onrender.com/api/auth', // ✅ Change if your backend runs on a different port or subdomain
   withCredentials: true, // 🧠 Sends/receives cookies like preAuthToken and token
 });
 
