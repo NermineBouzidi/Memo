021409004b4785a1972321f3bc40868b649a4260
Nermine Bouzidi <101474434+NermineBouzidi@users.noreply.github.com>
2025-07-29 12:35:54 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.use(cors({
 app.use((req, res, next) => {
   const origin = process.env.CLIENT_URL || 'https://memo-z241.vercel.app';
 
-  res.setHeader('Access-Control-Allow-Origin', https://memo-1ppr.onrender.com);
+  res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
