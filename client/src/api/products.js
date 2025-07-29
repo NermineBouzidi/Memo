c7db00649efa65b01e038ac86685b63d24774867
Nermine Bouzidi <101474434+NermineBouzidi@users.noreply.github.com>
2025-07-29 09:59:08 +0100

    Update products.js

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/products', // ✅ Change if your backend runs on a different port or subdomain
+  baseURL: 'https://memo-1ppr.onrender.com/api/products', // ✅ Change if your backend runs on a different port or subdomain
   withCredentials: true, // 🧠 Sends/receives cookies like preAuthToken and token
 });
 
